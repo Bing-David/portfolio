391128e0bc5e3d1d6970189c46ab04d06f3f5f90
UltimoDavid <69020315+Bing-David@users.noreply.github.com>
2024-01-14 15:36:07 -0500

    Frist

---
+++ src/index.js
@@ -0,0 +1,59 @@
+let element = document.getElementById('card_01');
+let element2 = document.getElementById('card_02');
+let element3 = document.getElementById('github');
+let element4 = document.getElementById('contact');
+
+
+element.addEventListener("click", ()=>{
+    window.open('https://github.com/Bing-David/Inventario-APP')
+});
+
+element2.addEventListener("click", ()=>{
+    window.open('https://github.com/Bing-David/Keyboard-Test')
+});
+
+
+element3.addEventListener("click", ()=>{
+    window.open('https://github.com/Bing-David')
+});
+
+element4.addEventListener("click", ()=>{
+  window.open('mailto:david2003ytjd@gmail.com')
+
+});
+
+// // Función para mostrar el modal
+// function openModal() {
+//     document.getElementById('resolutionModal').style.display = 'block';
+//   }
+  
+//   // Función para cerrar el modal
+//   function closeModal() {
+//     document.getElementById('resolutionModal').style.display = 'none';
+//   }
+  
+//   // Función para actualizar la resolución en el modal
+//   function updateResolution() {
+//     const resolutionText = document.getElementById('resolutionValue');
+//     resolutionText.textContent = `${window.innerWidth} x ${window.innerHeight}`;
+//   }
+  
+//   // Mostrar el modal al cargar la página
+//   window.onload = function() {
+//     openModal();
+//     updateResolution();
+//   };
+  
+//   // Actualizar la resolución cuando cambia el tamaño de la ventana
+//   window.onresize = function() {
+//     updateResolution();
+
+
+{/* <div class="modal" id="resolutionModal">
+<span class="close" onclick="closeModal()">&times;</span>
+<p id="resolutionText"><span id="resolutionValue"></span></p>
+</div> */}
+
+
+//   };
+  
